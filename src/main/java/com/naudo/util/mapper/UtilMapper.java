7896c0e12c53eaf237d20fa31255958830921ed6
naudofp <felipenaudof@gmail.com>
2023-03-15 16:40:03 -0300

    Updated UtilMapper

--- src/main/java/com/naudo/util/mapper/UtilMapper.java
+++ src/main/java/com/naudo/util/mapper/UtilMapper.java
@@ -3,8 +3,11 @@ package com.naudo.util.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.naudo.dto.order.OrderDTO;
 import com.naudo.dto.orderitem.ItemDetailsDTO;
+import com.naudo.dto.orderitem.OrderItemDTO;
 import com.naudo.dto.product.ProductCardDTO;
+import com.naudo.model.order.Order;
 import com.naudo.model.orderitem.OrderItem;
 import com.naudo.model.product.Product;
 import com.naudo.service.image.ImageService;
@@ -32,5 +35,21 @@ public class UtilMapper {
 		items.forEach(item -> itemsDto.add(new ItemDetailsDTO(item.getId(), eProductToProductCardDTO(item.getProduct()), item.getQuantity())));
 		
 		return itemsDto;
+	}
+	
+	public static List<OrderItemDTO> eAllOrderItemsToAllOrderItemDTO(List<OrderItem> items){
+		List<OrderItemDTO> itemsDto = new ArrayList<>();
+		items.forEach(item -> itemsDto.add(new OrderItemDTO(item.getProduct().getId(), item.getQuantity())));
+		return itemsDto;
+	}
+	
+	public static OrderDTO eOrderToOrderDTO(Order entity) {
+		return new OrderDTO(entity.getId(), entity.getIssueDate(), entity.getStatus(), eAllOrderItemsToAllOrderItemDTO(entity.getItems()), entity.getAmount());
+	}
+	
+	public static List<OrderDTO> eAllOrderToAllOrderDTO(List<Order> orders) {
+		List<OrderDTO> ordersDto = new ArrayList<>();
+		orders.forEach(item -> ordersDto.add(eOrderToOrderDTO(item)));
+		return ordersDto;
 	}
 }
